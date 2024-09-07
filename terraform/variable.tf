199d3d01ebc3099fed6eaba8372280bdf7260105
pradeepreddy <pradeepreddy921@gmail.com>
2024-09-07 14:41:59 +0200

    added changes

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -51,14 +51,3 @@ variable "cluster_name" {
 #   default = "ap-south-1"
 # }
 
-variable "new_relic_license_key" {
-  description = "New Relic License Key"
-  type        = string
-  default = "eu01xx6e7ede4009aa1ff7a86f07714dFFFFNRAL"
-}
-
-variable "cluster_name" {
-  description = "Name of the EKS Cluster"
-  type        = string
-  default = "eks-cluster"
-}
